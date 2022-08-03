d85d9e767c1f1da5ea007af2617513573982515a
Roshan Kumar Sharma <92990143+RoshanKrSharma@users.noreply.github.com>
2022-08-03 23:28:28 +0530

    Update 13. Investments in 2016.sql

--- 13. Investments in 2016.sql
+++ 13. Investments in 2016.sql
@@ -29,3 +29,42 @@ policyholder(i.e.,the(lat, lon) attribute pairs
 must be unique).
 
 Round tiv_2016 to two decimal places.
+example:
+-- Sample Input
+Table: Insurance
+
+-- | PID | TIV_2015 | TIV_2016 | LAT | LON |
+-- |-----|----------|----------|-----|-----|
+-- | 1   | 10       | 5        | 10  | 10  |
+-- | 2   | 20       | 20       | 20  | 20  |
+-- | 3   | 10       | 30       | 20  | 20  |
+-- | 4   | 10       | 40       | 40  | 40  |
+-- Sample Output
+
+-- | TIV_2016 |
+-- |----------|
+-- | 45.00    |
+-- Explanation
+
+-- The first record in the table, like the last record, meets both of the two criteria.
+-- The TIV_2015 value '10' is as the same as the third and forth record, and its location unique.
+
+-- The second record does not meet any of the two criteria. Its TIV_2015 is not like any other policyholders.
+
+-- And its location is the same with the third record, which makes the third record fail, too.
+
+-- Solution
+select sum(TIV_2016) TIV_2016
+from 
+(select *, count(*) over (partition by TIV_2015) as c1, count(*) over (partition by LAT, LON) as c2
+from insurance ) t
+where c1 > 1 and c2 = 1; 
+
+alernate solution: 
+select round(
+from Insurance
+where TIV_2015 in (select TIV_2015 Insurance groupt by TIV_2015) and
+concat lat,lon in
+(select concat lat,lon) from Insurance
+group by concat lat, lon)
+having count(*) =1);
