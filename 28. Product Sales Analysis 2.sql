02ec15676fdca5bde19f43fdbd8612eb5b489a4d
Roshan Kumar Sharma <92990143+RoshanKrSharma@users.noreply.github.com>
2022-08-20 15:24:33 +0530

    Update 28. Product Sales Analysis 2.sql

--- 28. Product Sales Analysis 2.sql
+++ 28. Product Sales Analysis 2.sql
@@ -1,2 +1,16 @@
 -- see question in OneNote
 
+-- Solution
+Select a.product_id, sum(a.quantity) as total_quantity
+from sales a
+join
+product b
+on a.product_id = b.product_id
+group by a.product_id
+
+
+-- alternate solution
+select product_id,sum(quantity) as total quantity
+from sales
+group by product_id
+
