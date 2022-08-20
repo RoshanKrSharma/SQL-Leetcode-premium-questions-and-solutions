a409111f5e97813d17172684ee27e0b66570763e
Roshan Kumar Sharma <92990143+RoshanKrSharma@users.noreply.github.com>
2022-08-20 09:10:32 +0530

    Update 24. Biggest single number.sql

--- 24. Biggest single number.sql
+++ 24. Biggest single number.sql
@@ -1,3 +1,15 @@
 -- see question in OneNote
 
--- 
+-- alternate solution
+
+Select max(num) as num
+from 
+( 
+    select num
+    from my_numbers
+    group by num
+    having count(num)=1
+) t1
+
+-- yaha alias de k t1.num krte select mein to v wrong ni hota
+
